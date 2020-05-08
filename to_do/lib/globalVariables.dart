cf226e4844c028e786898207d4ad32d2fb3927a4
Adithya Shetty <adithyashetty032@gmail.com>
2020-05-08 23:41:35 +0530

    rules for global variables ddeclaration added

--- to_do/lib/globalVariables.dart
+++ to_do/lib/globalVariables.dart
@@ -1,1 +1,6 @@
+///This is a global variable file.
+///Only variables that are intended to use outside the scope of the class and the functions
+///should be listed here
+///No new functions or methods or class should be created inside this file.
+
 int minimisedListCounter = 20;
