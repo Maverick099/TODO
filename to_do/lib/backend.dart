5ba86dac92d18f4ee18ffa3d757c9a70bd69d254
Adithya Shetty <adithyashetty032@gmail.com>
2020-05-08 23:40:47 +0530

    new backend file added

---
+++ to_do/lib/backend.dart
@@ -0,0 +1,14 @@
+/// The approach is to give a unique id and coulums in the id a unique digit.
+/// And store the combination in and array and the combination will be assigned bollean 
+/// value according to the check mark for the each colum and card.
+///
+/// second approach to intialise and array named with the name 'id' of the card 
+/// and the column status will be the part of the dictonanry
+/// 
+/// 
+/// For the time calculations the current device time will be registered and then the according
+/// the number of task in the card the time will be divided evenly or the use can choose the time.
+/// while dividing the time the users misc task time  from the pre recorded and will be deducted from 
+/// his daily time and then the time for the task in the card will be divided.
+
+
